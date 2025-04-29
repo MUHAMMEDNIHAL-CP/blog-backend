1ab0889c4d8ad6f42775bff01854a07e8968c227
Nihal <muhdnihal3200@gmail.com>
2025-04-29 22:28:41 +0530

    Fixed

--- blog_project/urls.py
+++ blog_project/urls.py
@@ -17,6 +17,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
@@ -25,3 +28,7 @@ urlpatterns = [
     path('api/', include('users.urls')),  # User-related API routes (like register, login, etc.)
     path('api/blogs/', include('blogs.urls')),  # Blog-related API routes (list, create, detail, etc.)
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
